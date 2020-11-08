747a3db4058f44aab8f49af06b674cf1bee6ad6e
stepinfwd <stepinfwd@gmail.com>
2020-11-08 16:25:05 +0530

    logo added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./Header.css";
 import Navigation from "../Navbar/Navigation";
+import logo from "../logo/icons8-blockchain-new-logo.svg";
 
 function Header() {
   return (
     <div className="header-container">
-      <span>AppName</span>
+      <div>
+        <img src={logo}></img>
+        <span className="header-container-appname">AppName</span>
+      </div>
       <Navigation />
     </div>
   );
--- src/components/Navbar/NavigationMenu.js
+++ src/components/Navbar/NavigationMenu.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./Navbar.css";
+import logo from "../logo/icons8-blockchain-new-logo.svg";
 
 function NavigationMenu(props) {
   const { triggerClose } = props;
 
   return (
     <div className="navigation-menu-container">
-      <div className="navigation-menu-heading">MENU</div>
+      <div className="navigation-menu-heading">
+        <img src={logo}></img>Logo
+      </div>
       <ul>
         <li>
           <Link className="nav-links" onClick={props.triggerClose} to="/">
@@ -30,7 +33,7 @@ function NavigationMenu(props) {
         </li>
       </ul>
       <div className="sidebar-footer">
-        <p>Stepinfwd&copy; 2020</p>
+        <p>stepinfwd&copy; 2020</p>
       </div>
     </div>
   );
